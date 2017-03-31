e251ccc59adff452271a6d5163e9bd5f8b1c521e
gatsunoko <gatsunoko@yahoo.co.jp>
2017-03-31 09:16:03 +0900

    cropper追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'images#index'
+  resources :images do
+    collection do
+      post :upload
+    end
+  end
 end
